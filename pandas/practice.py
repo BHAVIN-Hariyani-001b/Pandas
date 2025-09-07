20f46985274d5a37bb08e08e9f42b4f8874095f7
BHAVIN-Hariyani-001b <bhedit367@gmail.com>
2025-09-07 21:39:25 +0530

    pandas file

---
+++ pandas/practice.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# READ CSV FILE *******
+# df = pd.read_csv('./sales_data_sample.csv',encoding='latin1')
+# df = pd.read_csv('./sales.csv',encoding='latin1 OR utf-8') # error throw not read csv file and encode error and write to use
+# print(df)
+
+# df = pd.read_excel('./SampleSuperstore.xlsx',engine='openpyxl')
+# print(df)
